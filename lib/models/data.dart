24afe33fad5d71fcb476283b888175712705b73d
aymen.ziouche <aymen.zch23@gmail.com>
2022-02-01 21:53:26 +0100

    fixed loading data prob

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,3 +1,29 @@
+// To parse this JSON data, do
+//
+//     final medias = mediasFromMap(jsonString);
+
+import 'dart:convert';
+
+class Medias {
+  Medias({
+    required this.data,
+  });
+
+  List<Media> data;
+
+  factory Medias.fromJson(String str) => Medias.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Medias.fromMap(Map<String, dynamic> json) => Medias(
+        data: List<Media>.from(json["data"].map((x) => Media.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "data": List<dynamic>.from(data.map((x) => x.toMap())),
+      };
+}
+
 class Media {
   Media({
     required this.id,
@@ -6,19 +32,23 @@ class Media {
     required this.timestamp,
   });
 
-  int id;
+  String id;
   String caption;
   String mediaUrl;
   String timestamp;
 
-  factory Media.fromJson(Map<String, dynamic> json) => Media(
-        id: json["id"],
-        caption: json["caption"],
-        mediaUrl: json["media_url"],
-        timestamp: json["timestamp"],
+  factory Media.fromJson(String str) => Media.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Media.fromMap(Map<String, dynamic> json) => Media(
+        id: json["id"] ?? "",
+        caption: json["caption"] ?? "",
+        mediaUrl: json["media_url"] ?? "",
+        timestamp: json["timestamp"] ?? "",
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toMap() => {
         "id": id,
         "caption": caption,
         "media_url": mediaUrl,
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:insta_details/models/data.dart';
@@ -16,8 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with CustomDioMixin {
   bool loading = true;
   bool error = false;
-  late Media media;
-
+  List<Media> media = [];
   @override
   void initState() {
     super.initState();
@@ -27,20 +25,30 @@ class _HomePageState extends State<HomePage> with CustomDioMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: loading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : error
+      body: Column(
+        children: [
+          Expanded(
+            child: loading
                 ? const Center(
-                    child: Text('An error has occurred!'),
+                    child: CircularProgressIndicator(),
                   )
-                : ListView.builder(
-                    itemBuilder: (context, index) => MediaWidget(
-                      media: media,
-                    ),
-                  ),
+                : error
+                    ? const Center(
+                        child: Text('An error has occurred!'),
+                      )
+                    : ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: 6,
+                        itemBuilder: (context, index) {
+                          print("media n° $index ${media[index].mediaUrl}");
+                          return MediaWidget(
+                            media: media[index],
+                          );
+                        },
+                      ),
+          ),
+        ],
       ),
     );
   }
@@ -49,12 +57,14 @@ class _HomePageState extends State<HomePage> with CustomDioMixin {
     try {
       final storage = GetStorage();
       final token = storage.read("accessToken");
-      Media? media;
 
       final response = await dio.get(
         'https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp&access_token=$token',
       );
       print("get data response => ${response.statusCode} ${response.data}");
+      if (response.statusCode.toString().startsWith('2')) {
+        media = Medias.fromMap(response.data).data;
+      }
     } catch (e) {
       print("get data failed");
       print(e);
@@ -70,18 +80,25 @@ class _HomePageState extends State<HomePage> with CustomDioMixin {
 }
 
 class MediaWidget extends StatelessWidget {
-  final Media media;
+  final Media? media;
   const MediaWidget({Key? key, required this.media}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+    return SizedBox(
+      height: 500,
+      width: 500,
+      child: GridView.builder(
+        shrinkWrap: true, //
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          return media != null && media!.mediaUrl != ""
+              ? Image.network(media!.mediaUrl)
+              : SizedBox.shrink();
+        },
       ),
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return Image.network(media.mediaUrl);
-      },
     );
   }
 }
+// well, as u can see i cant get the media url. it's null. why is that happenning?
